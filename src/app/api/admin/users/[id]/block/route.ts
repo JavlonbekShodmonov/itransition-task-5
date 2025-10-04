3bcbab7ad230f75574a71aa6880e73437eae2aef
Javlon <mohiiish.com@gmail.com>
2025-10-04 19:53:15 +0500

    other one

--- src/app/api/admin/users/[id]/block/route.ts
+++ src/app/api/admin/users/[id]/block/route.ts
@@ -4,11 +4,11 @@ import User from "../../../../../../models/User";
 
 export async function PATCH(
   req: NextRequest,
-  context: { params: { id: string } } // must be exactly like this
+  context: { params: Promise<{ id: string }> }
 ) {
   await dbConnect();
 
-  const { id } = context.params;
+  const { id } = await context.params;
 
   if (!id) {
     return NextResponse.json({ message: "User ID is required" }, { status: 400 });
